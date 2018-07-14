caffab868526b5fac13c82a0b2a361d5616cbc96
wayne <tw0988519746@hotmail.com>
2018-07-14 10:51:31 +0800

    練習 ViewData, ViewBag, TempData 的各種用法

---
+++ MVC5Course/Controllers/MBController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5Course.Controllers
+{
+    public class MBController : BaseController
+    {
+        public ActionResult Index()
+        {
+            ViewData.Model = "Hello";
+
+            return View();
+        }
+
+        public ActionResult ViewBagDemo()
+        {
+            ViewBag.Text = "hi";
+
+            return View();
+        }
+
+        public ActionResult ViewDataDemo()
+        {
+            ViewData["Text"] = "hi";
+
+            return View();
+        }
+
+        public ActionResult ViewTempSave()
+        {
+            TempData["Text"] = "Temp Data";
+
+            return RedirectToAction("ViewTempDemo");
+        }
+
+        public ActionResult ViewTempDemo()
+        {
+
+            return View();
+        }
+    }
+}
